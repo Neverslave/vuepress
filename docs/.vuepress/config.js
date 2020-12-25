bb5894ef89d8f75be9081c7a65c160ba3d5f7f9e
Zhu <1029110566@qq.com>
2020-12-25 11:12:23 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,21 @@
 module.exports = {
-    title: '',
+    title: '个人文档总结',
     description: 'Just playing around',
     themeConfig: {
         nav: [
-          { text: 'mysql相关操作', link: '/file/mysql相关操作' },
-          { text: 'Oracle相关操作', link: '/file/Oracle 数据库相关操作' },
+            {
+                text:'数据库相关',
+                ariaLabel: 'DataBase Menu',
+                items: [
+                    {text: 'mysql相关操作', link: '/file/mysql相关操作'},
+                    { text: 'Oracle相关操作', link: '/file/Oracle 数据库相关操作' },
+                ]
+            },
+          { text: 'NC运维相关', link: '/file/NC运维相关' },
 
         ],
         sidebar: 'auto'
-      }
+      },
+    sidebarDepth: 2
+
   }
